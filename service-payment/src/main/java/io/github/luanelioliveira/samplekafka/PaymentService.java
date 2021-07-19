1de92b442e8bb3b5dc0121545d2243ac5eb6a5d2
Luan Eli Oliveira <luannn@gmail.com>
2021-07-19 19:56:50 -0300

    refact(payment): refactoring service runner

--- service-payment/src/main/java/io/github/luanelioliveira/samplekafka/PaymentService.java
+++ service-payment/src/main/java/io/github/luanelioliveira/samplekafka/PaymentService.java
@@ -1,29 +1,35 @@
 package io.github.luanelioliveira.samplekafka;
 
-import io.github.luanelioliveira.samplekafka.consumer.KafkaService;
+import io.github.luanelioliveira.samplekafka.consumer.ConsumerService;
+import io.github.luanelioliveira.samplekafka.consumer.ServiceRunner;
 import io.github.luanelioliveira.samplekafka.dispatcher.KafkaDispatcher;
 import java.math.BigDecimal;
 import java.util.concurrent.ExecutionException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-public class PaymentService {
+public class PaymentService implements ConsumerService<Order> {
 
+  private static final int THREADS = 5;
   private static final String ORDER_CREATED_TOPIC_NAME = "ORDER_CREATED";
   private static final String PAYMENT_APPROVED_TOPIC_NAME = "PAYMENT_APPROVED";
   private static final String PAYMENT_REJECTED_TOPIC_NAME = "PAYMENT_REJECTED";
 
-  public static void main(String[] args) throws DeadLetterUnavailableException {
-    var service = new PaymentService();
-
-    try (var kafkaService = new KafkaService(
-        PaymentService.class.getSimpleName(),
-        ORDER_CREATED_TOPIC_NAME,
-        service::consume)) {
-      kafkaService.run();
-    }
+  public static void main(String[] args) {
+    new ServiceRunner(PaymentService::new).start(THREADS);
   }
 
-  private void consume(ConsumerRecord<String, Message<Order>> record)
+  @Override
+  public String getConsumerGroup() {
+    return PaymentService.class.getSimpleName();
+  }
+
+  @Override
+  public String getTopic() {
+    return ORDER_CREATED_TOPIC_NAME;
+  }
+
+  @Override
+  public void consume(ConsumerRecord<String, Message<Order>> record)
       throws ExecutionException, InterruptedException {
     System.out.println("------------------------------------------");
     System.out.println("Processing payment order: " + record.value());
